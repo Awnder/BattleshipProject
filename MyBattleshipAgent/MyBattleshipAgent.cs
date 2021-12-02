1c25040257b0ec08a8626ffd39841ad85f4f9e56
Andrew <Andrew@DESKTOP-P4JQ66H>
2021-12-02 15:53:27 -0800

    Add project files.

---
+++ MyBattleshipAgent/MyBattleshipAgent.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Battleship
+{
+    public class SuperCoolAgent : BattleshipAgent
+    {
+        char[,] attackHistory;
+        GridSquare attackGrid;
+
+        public SuperCoolAgent()
+        {
+            attackHistory = new char[10, 10];
+            attackGrid = new GridSquare();
+        }
+
+        public override void Initialize()
+        {
+            return;
+        }
+
+        public override string ToString()
+        {
+            return $"Battleship Agent '{GetNickname()}'";
+        }
+
+        public override string GetNickname()
+        {
+            return "Ron Weasly";
+        }
+
+        public override void SetOpponent(string opponent)
+        {
+            return;
+        }
+
+        public override GridSquare LaunchAttack()
+        {
+            attackGrid.x = 5;
+            attackGrid.y = 5;
+            return attackGrid;
+        }
+
+        public override void DamageReport(char report)
+        {
+            attackHistory[attackGrid.x, attackGrid.y] = report;
+        }
+
+        public override BattleshipFleet PositionFleet()
+        {
+            BattleshipFleet myFleet = new BattleshipFleet();
+
+            myFleet.Carrier = new ShipPosition(0, 0, ShipRotation.Vertical);
+            myFleet.Battleship = new ShipPosition(1, 0, ShipRotation.Vertical);
+            myFleet.Destroyer = new ShipPosition(2, 0, ShipRotation.Vertical);
+            myFleet.Submarine = new ShipPosition(3, 0, ShipRotation.Vertical);
+            myFleet.PatrolBoat = new ShipPosition(4, 0, ShipRotation.Horizontal);
+
+            return myFleet;
+        }
+    }
+}
